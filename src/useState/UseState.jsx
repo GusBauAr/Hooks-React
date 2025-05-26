a116d39fd44406e9f367691a9bfd46158dd43df9
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-26 10:25:21 -0400

    update hook useState

--- src/useState/UseState.jsx
+++ src/useState/UseState.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import { useState } from "react"
 
 export const UseState = () => {
-    const [count, setCount] = useState(0)
+    const [contador, setContador] = useState(0);
   return (
     <>
-    <h1>useState - Count</h1>
-    <h1>{count}</h1>
-    <button onClick={() => setCount(count + 1)}>
-        click
+    <h1>{contador}</h1>
+    <button onClick={() => setContador ( contador + 100 )}>
+        Aumentar
     </button>
     </>
+
+    
   )
 }
+
 
